5ef7647c7df77e39cef3f6a40b3234184d794dd2
krish <krishnaja-b>
2023-04-15 22:17:49 +0530

    using functions in mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,16 @@
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+print_head(){
+echo -e "\e[36m$1\e[0m"
+}
+print_head "setup mogodb repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+print_head "install mongodb"
 yum install mongodb-org -y
+print_head "enable mongodb"
 systemctl enable mongod
+print_head "start mongodb"
 systemctl start mongod
 
 #update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
