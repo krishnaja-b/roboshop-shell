f81a7d08d2bec3ee5530efb89d8c898e0e62b565
krish <krishnaja-b>
2023-04-15 23:12:58 +0530

    catalouge file

--- catalouge.sh
+++ catalouge.sh
@@ -1,20 +1,35 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+source common.sh
+print_head "configure nodejs repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{log_file}
+print_head "install nodejs"
+yum install nodejs -y &>>{log_file}
+print_head "create roboshop user"
+useradd roboshop &>>{log_file}
+print_head "create application directory"
+mkdir /app &>>{log_file}
+print_head "delete old content"
+rm -rf /app/* &>>{log_file}
+print_head "downloading app content"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{log_file}
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp configs/catalouge.service /etc/systemd/system/catalouge.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.aws43.xyz </app/schema/catalogue.js
+print_head "extracting app content"
+unzip /tmp/catalogue.zip &>>{log_file}
+print_head "installing nodejs dependencies"
+npm install &>>{log_file}
+print_head "copy systemd service file"
+cp configs/catalouge.service /etc/systemd/system/catalouge.service &>>{log_file}
+print_head "reload systemd"
+systemctl daemon-reload &>>{log_file}
+print_head "enable catalouge service"
+systemctl enable catalogue &>>{log_file}
+print_head "start catalouge service"
+systemctl start catalogue &>>{log_file}
+print_head "copy mongodb repo file"
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>{log_file}
+print_head "install mongo clint"
+yum install mongodb-org-shell -y &>>{log_file}
+print_head "load schema"
+mongo --host mongodb.aws43.xyz </app/schema/catalogue.js &>>{log_file}
 
 
 
