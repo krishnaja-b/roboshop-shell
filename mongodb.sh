3b9bfa0d12706283f1c5f321d19e68f9eedd4d0f
krish <krishnaja-b>
2023-04-21 22:40:16 +0530

    using statuscheck finction in mondb

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,21 @@
 source common.sh
 print_head "setup mogodb repository"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
 print_head "install mongodb"
 yum install mongodb-org -y &>>${log_file}
+status_check $?
+
 print_head "update mongodb listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
+status_check $?
 print_head "enable mongodb"
 systemctl enable mongod &>>${log_file}
+status_check $?
+
 print_head "start mongodb service"
 systemctl restart mongod &>>${log_file}
-
+status_check $?
 
 
 
