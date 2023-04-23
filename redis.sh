07b2d704a6107458842d224ad3d0f789e5fd0692
krish <krishnaja-b>
2023-04-23 08:40:31 +0530

    redis component

---
+++ redis.sh
@@ -0,0 +1,27 @@
+source common.sh
+print_head "installing redis repo files"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
+status_check $?
+
+print_head "enable redis 6.2"
+dnf module enable redis:remi-6.2 -y &>>${log_file}
+status_check $?
+
+print_head "install redis"
+yum install redis -y  &>>${log_file}
+status_check $?
+
+print_head "update redis listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
+status_check $?
+
+print_head "enable redis service"
+systemctl enable redis  &>>${log_file}
+status_check $?
+
+print_head "start redis service"
+systemctl start redis &>>${log_file}
+status_check $?
+
+
+
