e7acbfffd7f29eb25316580edb2618508c4f081e
krish <krishnaja-b>
2023-04-28 00:47:11 +0530

    shipping n common

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,7 @@
 source common.sh
 mysql_root_password=$1
-if [ -z "${mysql_root_password}" == "mysql" ]; then
-    echo -e "\e[31m"missing mysql root password argument"\e[0m"
+if [ -z "${mysql_root_password}" ]; then
+    echo -e "\e[31mmissing mysql root password argument\e[0m"
     exit 1
 fi
 
