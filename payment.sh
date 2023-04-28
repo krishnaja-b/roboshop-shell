6ceb780b299cd56b7a872058b1a95cb9750079a3
krish <krishnaja-b>
2023-04-28 11:45:44 +0530

    payment

---
+++ payment.sh
@@ -0,0 +1,11 @@
+source common.sh
+roboshop_app_password=$1
+if [ -z "${roboshop_app_password}" ]; then
+echo -e "\e[31m"missing roboshop app password argument"\e[0m"
+exit 1
+fi
+
+component=payment
+python
+
+
